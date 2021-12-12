1a883cac1089ad5d35992e69662e3b82d1cb07ae
Mike7501 <nhatpm19411@st.uel.edu.vn>
2021-12-12 10:28:50 +0700

    Màn hình chỉnh sửa + thêm sản phẩm

---
+++ app/src/main/java/com/n03/g701/Product_Add.java
@@ -0,0 +1,14 @@
+package com.n03.g701;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Product_Add extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_add);
+    }
+}
