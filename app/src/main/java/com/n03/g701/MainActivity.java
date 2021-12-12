cff3024d58e29fa47a700bb2a6c9a1760620b85a
Mike7501 <nhatpm19411@st.uel.edu.vn>
2021-12-12 11:21:14 +0700

    Intent Main Activity to Add Product Class

--- app/src/main/java/com/n03/g701/MainActivity.java
+++ app/src/main/java/com/n03/g701/MainActivity.java
@@ -2,7 +2,10 @@ package com.n03.g701;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 
 import com.n03.g701.adapter.ProductAdapter;
@@ -15,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     ListView lvProduct;
     ArrayList<Product> arrayList;
     ProductAdapter productAdapter;
-
+    Button btnAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,15 +30,23 @@ public class MainActivity extends AppCompatActivity {
         initData();
     }
 
-    private void linkViews() {
+    private void linkViews()
+    {
         lvProduct= findViewById(R.id.lvProduct);
+        btnAdd = findViewById(R.id.btnAddProduct);
     }
 
     private void addEvents() {
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Product_Add.class);
+                startActivity(intent);
+            }
+        });
         productAdapter = new ProductAdapter(MainActivity.this, R.layout.item_listview, initData());
         lvProduct.setAdapter(productAdapter);
     }
-
     private ArrayList<Product> initData() {
 
         return arrayList;
